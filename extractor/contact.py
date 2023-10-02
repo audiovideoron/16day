61ff8a3f6b300d3d1c8d68a2050b8ff8b922634c
audiovideoron <audiovideoron@gmail.com>
2023-10-01 19:04:03 -0500

    cotacts is usable

--- extractor/contact.py
+++ extractor/contact.py
@@ -1,12 +1,20 @@
-import argparse
 import json
 import re
+from json.decoder import JSONDecodeError
 
-def is_valid_phone_number(phone):
-    pattern = r"^\(\d{3}\) \d{3}-\d{4}$"
-    return re.match(pattern, phone) is not None
-
-positions_dict = {
+# Constants
+PATTERN_PHONE = r"^\(\d{3}\) \d{3}-\d{4}$"
+POSITIONS = {
+    "a1": "Audio Level 1",
+    "a2": "Audio Level 2",
+    "v1": "Video Level 1",
+    "v2": "Video Level 2",
+    "l1": "Lighting Level 1",
+    "l2": "Lighting Level 2",
+    "crew_chief": "Crew Chief",
+    "hand": "Hand",
+}
+POSITION_WEIGHTS = {
     "a1": 2,
     "a2": 1,
     "v1": 2,
@@ -17,36 +25,32 @@ positions_dict = {
     "hand": 1,
 }
 
+def is_valid_phone_number(phone):
+    patterns = [
+        r"^\(\d{3}\) \d{3}-\d{4}$",  # (XXX) XXX-XXXX
+        r"^\d{3}-\d{3}-\d{4}$",      # XXX-XXX-XXXX
+        r"^\d{10}$",                 # XXXXXXXXXX
+        # Add more patterns as needed
+    ]
+
+    return any(re.match(pattern, phone) for pattern in patterns)
+
+def get_yes_no_input(prompt):
+    return input(prompt).strip().lower() == 'y'
+
 def calculate_weight(contact_positions, reliable, flexible, client_relations, preferred):
-    weight = sum(positions_dict[pos] for pos in contact_positions)
-    if reliable:
-        weight += 1
-    if flexible:
-        weight += 1
-    if client_relations:
-        weight += 1
-    if preferred:
-        weight += 1
+    weight = sum(POSITION_WEIGHTS[pos] for pos in contact_positions)
+    weight += reliable + flexible + client_relations + preferred
     return weight
 
 def gather_contact_info():
     contacts = []
-    positions = {
-        "a1": "Audio Level 1",
-        "a2": "Audio Level 2",
-        "v1": "Video Level 1",
-        "v2": "Video Level 2",
-        "l1": "Lighting Level 1",
-        "l2": "Lighting Level 2",
-        "crew_chief": "Crew Chief",
-        "hand": "Hand",
-    }
-
+    available_positions = POSITIONS.copy()
+    
     while True:
         first_name = input("Enter first name (or 'q' to quit): ")
         if first_name.lower() == 'q':
             break
-
         last_name = input("Enter last name: ")
 
         phone = ''
@@ -54,48 +58,45 @@ def gather_contact_info():
             phone = input("Enter phone number (in the format (XXX) XXX-XXXX): ")
             if is_valid_phone_number(phone):
                 break
-            else:
-                print("Invalid phone number format. Please use the format (XXX) XXX-XXXX.")
+            print("Invalid phone number format. Please use the format (XXX) XXX-XXXX.")
 
         contact_positions = []
-        available_positions = positions.copy()
         while True:
             if not available_positions:
                 print("No more positions available.")
                 break
-
-            print("Choose position(s) from the following list (enter 'done' to finish):")
+            print("Choose position(s) from the following list (enter 'done' to finish).")
             for pos, desc in available_positions.items():
                 print(f"{pos}: {desc}")
 
-            position_input = input("Enter a position: ").strip()
+            position_input = input("Enter one or multiple positions separated by commas: ").strip().replace(" ", "")
             if position_input == 'done':
                 break
-
-            if position_input in available_positions:
-                contact_positions.append(position_input)
-
-                if position_input in ["a1", "v1", "l1"]:
-                    corresponding_position = position_input[0] + "2"
-                    del available_positions[corresponding_position]
-
-                del available_positions[position_input]
+            selected_positions = position_input.split(',')
+            valid_positions = all(pos in available_positions for pos in selected_positions)
+            if valid_positions:
+                contact_positions.extend(selected_positions)
+                for pos in selected_positions:
+                    del available_positions[pos]
             else:
-                print("Invalid position. Please choose from the list.")
+                print("Invalid position(s). Please choose from the list.")
 
-        is_reliable = input(f"Is {first_name} reliable? (y/n): ").strip().lower() == 'y'
-        is_flexible = input(f"Is {first_name} flexible? (y/n): ").strip().lower() == 'y'
-        has_client_relations = input(f"Does {first_name} have good rapport with the client? (y/n): ").strip().lower() == 'y'
-        is_preferred = input(f"Do you prefer {first_name}? (y/n): ").strip().lower() == 'y'
+        is_reliable = get_yes_no_input(f"Is {first_name} reliable? (y/n): ")
+        is_flexible = get_yes_no_input(f"Is {first_name} flexible? (y/n): ")
+        has_client_relations = get_yes_no_input(f"Does {first_name} have good rapport with the client? (y/n): ")
+        is_preferred = get_yes_no_input(f"Do you prefer {first_name}? (y/n): ")
 
         weight = calculate_weight(contact_positions, is_reliable, is_flexible, has_client_relations, is_preferred)
 
         rate = 0.0
         while True:
-            rate = input("Enter rate: ")
+            rate_input = input("Enter rate: ")
             try:
-                rate = float(rate)
-                break
+                rate = float(rate_input)
+                if rate < 0:
+                    print("Rate must be positive. Try again.")
+                else:
+                    break
             except ValueError:
                 print("Invalid rate. Please enter a numeric rate.")
 
@@ -111,7 +112,6 @@ def gather_contact_info():
             'client_relations': has_client_relations,
             'preferred': is_preferred
         }
-        
         contacts.append(contact)
     return contacts
 
@@ -120,13 +120,10 @@ def save_to_json(contacts, filename="contact.json"):
         json.dump(contacts, json_file, indent=4)
 
 def main():
-    parser = argparse.ArgumentParser(description="Gather contact info and save to contact.json.")
-    args = parser.parse_args()
-
     try:
         with open("contact.json", "r") as json_file:
             existing_contacts = json.load(json_file)
-    except FileNotFoundError:
+    except (FileNotFoundError, JSONDecodeError):
         existing_contacts = []
 
     new_contacts = gather_contact_info()
--- readme.txt
+++ readme.txt
@@ -1,30 +1,22 @@
 Job Tracker
 
-Usable with features 1 and 2. Ron Parker @0479, DET and Job Tracker 
+Usable with features 1, 2 and 3. Ron Parker @0479, DET and Job Tracker 
 developer is the first user. Introduce software to Scott McCann and 
 West Region DETs to solicit a second user, feedback and feature requests. 
 
 Course of Development
 1. Add events to Outlook calendar
     A. Event Start, Event End
+Done!
 
-2. Schedule labor
+2. Schedule labor (per event for cost analysis)
     Script asks, 
     event_date = "What date?"
     event_name = "What is the event name?" (pick from list)
-    event_set = "What day are you setting?"
-    positions = "What positions are you looking for?" (choose one or more)
-    total_weight = :
-    * a1 = 1
-    * v1 = 1
-    * l1 = 1
-    * flexible = 1
-    * det_preference = 2
-    * client_relations = 1
-    * reschedules = -1
-    * no_show = -2
-    * engaged = -1
-    if v1 
+    event_set_date = "What day are you setting 'event_name'?"
+    positions = "What positions are you looking for?" (choose one or more, and how many for that position)
+
+   if positions v1, hand(3), crew_chief
     prospect = v1.labor.config && highest weight available
     for prospects
         if contact not booked on event (read calendar)
@@ -34,38 +26,20 @@ Course of Development
 
 
     Priority weight; if v1 and greater weight then message
-    before v1 priority 2.
+    before v1 with lesser weight.
 
-    A. Twilio to automate labor messaging
-    B. Start/End times are subject to change
+3. Add contacts
+    consider modifying is_valid_phone_number(phone): to format to e164
+    a seperate conversion function in our twilio code is probably preferable
+Done.
+
+Twilio 
+    Automate labor messaging
+    Disclaimer
+        Start/End times are subject to change
 3. Generate folders under documents/2023/current_month/client_name
 4. Read BEOs into ChatGPT for summation, 
 5. Bring Job Tracker to BEO meetings
 
-State of Development
-1. usable, but Start/End isn't infallible
-2. has been started and development will begin focusing on it
-3. not required for first release
-4. not required for first release
-5. close
-6. Inspire needs to buy Salesforce License for every site
-7. Inspire needs to buy license for twilio
-
 Notes:
 4. requires python to ChaptGPT API calls and needs to be paid for by Inspire.
-
-Early Version
-
-SMS
-The body of the calendar entry must follow this format. Any deviation breaks the interface.
-
-date_range: 2023-09-11 – 2023-09-13
-1 lead; Josiah, Dana, Phoenix
-1 hand; Joe Sullivan, Logan, Lisa
-
-date_range: 2023-09-11
-1 lead; Josiah, Dana
-1 hand; Logan, Lisa
-
-TODO
-1. Design updates for contacts
